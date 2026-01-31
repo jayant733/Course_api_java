a1a970719cccedf2c4d219f4b27a0b24a2b84f15
Jayant Sharma <jayant.sharma2023@vitstudent.ac.in>
2026-01-31 22:25:21 +0530

    feat: add repository search query for subtopics

--- course-api/src/main/java/com/courseplatform/course_api/repository/SubtopicRepository.java
+++ course-api/src/main/java/com/courseplatform/course_api/repository/SubtopicRepository.java
@@ -1,8 +1,18 @@
 package com.courseplatform.course_api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.courseplatform.course_api.model.Subtopic;
 
 public interface SubtopicRepository extends JpaRepository<Subtopic, String> {
+
+    @Query("""
+           SELECT s FROM Subtopic s
+           WHERE LOWER(s.title) LIKE LOWER(CONCAT('%', :query, '%'))
+              OR LOWER(s.content) LIKE LOWER(CONCAT('%', :query, '%'))
+           """)
+    List<Subtopic> searchSubtopics(String query);
 }
